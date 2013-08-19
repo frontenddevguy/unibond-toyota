c619f3642c6fbc2e3c81c1b9811e1ebba3e86291
frontenddevguy <hello@yanwhite.com>
2013-08-19 19:39:14 +0700

    end of day commit

---
+++ closed.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8">
+	<style type="text/css">
+		#wrap {display:table;height:100%}
+	</style>
+ <![endif]-->
+
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <!-- <link rel="stylesheet" href="css/normalize.css"> -->
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+
+    <body class="closed">
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        
+		<header>
+			<h1>Unibond</h1>
+		</header>
+		
+		<div id="wrap">
+			<section class="gradient">
+			
+				<div class="rotate">
+					<a href="url1">
+						<div class="win-proace choice gradient">
+							<h2>
+								<span class="its">It's</span> 
+								<span class="win">Win</span> 
+								<span class="vehicle-name">a Toyota Proace</span>
+							</h2>
+					
+							<img class="vehicle" src="img/van.png" alt="">
+							
+							<span class="or">or</span>
+						</div>
+					</a>
+			
+					
+			
+					<a href="url2">
+						<div class="win-hilux choice gradient">
+							<h2>
+								<span class="win">Win</span> 
+								<span class="vehicle-name">a Toyota Hilux</span>
+							</h2>
+					
+							<img class="vehicle" src="img/hilux.png" alt="">
+							
+							<img class="toyota-logo" src="img/logo_toyota.png" alt="">
+					
+						</div>
+					</a>
+					
+					<h4>Two Big Prizes to be Won with <span class="hidden">Unibond</span></h4>
+				</div>
+			
+				<div class="closed-banner">
+					<h3>Competition Closed</h3>
+				</div>
+			
+
+			</section>
+		</div>
+		
+		
+
+		
+		<?php include 'includes/footer.php'; ?>
+
+		
+		
+		
+		
+		
+		
+		
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+		<script type="text/javascript">
+ 
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-21748799-5']);
+		  _gaq.push(['_trackPageview']);
+ 
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+ 
+		</script>
+    </body>
+</html>
